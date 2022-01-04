fcab9929aadd200dea14ddc7aefec3de079ee6ea
Saurabh Krishan <saurabh.krishan@scaler.com>
2022-01-04 11:47:33 +0530

    added ability to mark todo items as complete, and added a bit of styling to show the complete status

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,15 +1,11 @@
 class TodoItemsController < ApplicationController
   before_action :set_todo_list
+  before_action :set_todo_item, except: [:create]
 
   def create
     @todo_item = @todo_list.todo_items.create(todo_item_params)
     redirect_to @todo_list
   end
-
-  # def show
-  #   @todo_list = TodoList.find(params[:todo_list_id])
-  #   # redirect_to @todo_list
-  # end
   
   def destroy
     @todo_item = @todo_list.todo_items.find(params[:id])
@@ -21,10 +17,19 @@ class TodoItemsController < ApplicationController
     redirect_to @todo_list 
   end
 
+  def complete
+    @todo_item.update_attribute(:completed_at, Time.now)
+    redirect_to @todo_list, notice: "Todo item completed"
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_todo_list
       @todo_list = TodoList.find(params[:todo_list_id])
+    end
+
+    def set_todo_item
+      @todo_item = @todo_list.todo_items.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
